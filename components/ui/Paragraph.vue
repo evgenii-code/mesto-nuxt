39a87fa4a1352163545c3f1a2d96cada6f43337e
evgenii-code <osipenkov.evgenii@gmail.com>
2020-09-14 18:08:03 +0300

    Error message popup

---
+++ components/ui/Paragraph.vue
@@ -0,0 +1,22 @@
+<template>
+  <p class="paragraph">{{ text }}</p>
+</template>
+
+<script>
+export default {
+  props: {
+    text: {
+      type: String,
+      default: 'Текст',
+      required: true,
+    },
+  },
+};
+</script>
+
+<style scoped>
+.text {
+  padding: 0;
+  margin: 0;
+}
+</style>
